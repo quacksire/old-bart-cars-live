12dff4d47402d38ad343495ed19b0f68111c114c
Quacksire <sam@quacksire.dev>
2023-09-20 23:56:37 -0700

    spelling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center">
 			<h1 className="font-bold text-center">
-				This list notes the next stop that a legacy train will make and its status.
+				This list notes the next stop that a legacy train will make and its ETA.
 			</h1>
 			<TrainList />
 		</section>
