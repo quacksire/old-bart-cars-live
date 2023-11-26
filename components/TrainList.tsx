3ad8efb7b5b959a2c6df26e4d6edda607f9f2809
Quacksire <sam@quacksire.dev>
2023-11-26 13:46:10 -0800

    modal go brrrr
    Signed-off-by: Quacksire <sam@quacksire.dev>

--- components/TrainList.tsx
+++ components/TrainList.tsx
@@ -1,23 +1,11 @@
 'use client'
 import {useEffect, useState} from "react";
 import moment from "moment";
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
-import {bartStationIDs} from "@/config/lists";
-import CountdownArrival from "@/components/CountdownArrival";
-import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    useDisclosure
-} from "@nextui-org/modal";
-import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/table";
-import {Snippet} from "@nextui-org/snippet";
+import TrainListItem from "@/components/TrainListItem";
 
 
 export default function TrainList() {
     const [timings, setTimings] = useState([])
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
     useEffect(() => {
         async function getTimings() {
@@ -65,128 +53,8 @@ export default function TrainList() {
 
     return (
         <div className={'grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1'}>
-            {timings.map((train: any) => {
-                // @ts-ignore
-
-                console.log(train)
-
-                let dep = moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000);
-                let arvl;
-                let diffInSeconds;
-
-                if (train.stopTimeUpdate[0]?.arrival?.time) {
-                    arvl = moment(parseInt(train.stopTimeUpdate[0].arrival.time) * 1000);
-                    diffInSeconds = dep.diff(arvl, 'minute'); // Difference in seconds
-                } else {
-                    diffInSeconds = null;  // or 'undefined', or 'N/A', or any other value you'd see fit
-                }
-
-                console.log(`Difference in minutes: ${diffInSeconds}`);
-
-
-                // @ts-ignore
-                if (moment(parseInt(train.stopTimeUpdate[0].arrival.time) * 1000).isBefore(moment())) {
-                    return
-                }
-
-
-
-
-                console.log(train)
-
-                // @ts-ignore
-                let arrivalTime = moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000)
-
-                // @ts-ignore
-                if (train?.delayed && train.stopTimeUpdate[0].departure.delay < 1) {
-                    // @ts-ignore
-                    train.delayed = false
-                }
-
-                // @ts-ignore
-                let lastStop = bartStationIDs[String(train.stopTimeUpdate[train.stopTimeUpdate.length - 1].stopId).toLowerCase()]
-
-                //
-                return (
-                    <>
-                    {/*@ts-ignore*/}
-                    <Card className="w-[300px] m-3 mr-3 rounded-xl " isPressable isHoverable key={train.trip.tripId} onPress={onOpen}>
-                        <CardBody className="flex gap-3">
-                            <div className="flex flex-col">
-                                {/*@ts-ignore*/}
-                                <p className="text-md">{bartStationIDs[String(train.stopTimeUpdate[0].stopId).toLowerCase()]}</p>
-
-                                <p className="text-small text-default-500">
-                                    <CountdownArrival arrivalTime={arrivalTime} />
-                                    {/*@ts-ignore*/}
-                                    {train?.delayed && <span className="text-red-500"> +{parseInt(train.stopTimeUpdate[0].departure.delay / 6).toFixed(0)} minutes late</span>}
-
-                                </p>
-                                {/*@ts-ignore*/}
-                                <p className="text-small text-default-500">{moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000).fromNow()}</p>
-
-                            </div>
-                        </CardBody>
-                        <CardFooter className="text-small text-default-500 text-center w-max">
-
-                            {!lastStop && (
-                                <>
-                                    Departing from this station
-                                </>
-                            )}
-                            {lastStop && (
-                                <>
-                                    {/*@ts-ignore*/}
-                                    Going to {bartStationIDs[String(train.stopTimeUpdate[train.stopTimeUpdate.length - 1].stopId).toLowerCase()]}
-                                </>
-                            )}
-
-                        </CardFooter>
-
-                    </Card>
-                        <Modal isOpen={isOpen} onOpenChange={onOpenChange} key={`${train.trip.tripId}-m`} scrollBehavior={'inside'} className={""}>
-                            <ModalContent>
-                                {(onClose) => (
-                                    <>
-                                        <ModalHeader className="flex flex-col gap-1">Trip #{train.trip.tripId}</ModalHeader>
-                                        <ModalBody>
-                                            <Table hideHeader isCompact removeWrapper fullWidth>
-                                                <TableHeader>
-                                                    <TableColumn width={0.5}>Stop Sequence</TableColumn>
-                                                    <TableColumn>T</TableColumn>
-                                                    <TableColumn>Station</TableColumn>
-                                                </TableHeader>
-                                                <TableBody>
-                                                    {train.stopTimeUpdate.map((stopTime: any) => {
-                                                        let delayed = false
-                                                        if (stopTime.departure.delay > 0) {
-                                                            // @ts-ignore
-                                                            delayed = true
-                                                        }
-
-                                                        return (
-                                                            <TableRow key={stopTime.stopId} className={'text-xs m-0'}>
-                                                                <TableCell className={'text-tiny text-foreground-400 '}>{stopTime.stopSequence} </TableCell>
-                                                                <TableCell>
-                                                                            <Snippet symbol="" variant="flat" color={delayed ? "danger" : "default"} hideCopyButton>{moment(parseInt(stopTime.departure.time) * 1000).toDate().toLocaleTimeString()}</Snippet>
-
-                                                                </TableCell>
-                                                                {/* @ts-ignore */}
-                                                                <TableCell>{bartStationIDs[String(stopTime.stopId).toLowerCase()]}</TableCell>
-                                                            </TableRow>
-                                                        )
-                                                    })}
-                                                </TableBody>
-                                            </Table>
-                                        </ModalBody>
-                                    </>
-                                )}
-                            </ModalContent>
-                        </Modal>
-                    </>
-                )
-
-            })}
+            {/* @ts-ignore */}
+            {timings.map((train: any) => <TrainListItem train={train} key={train.trip.tripId} />)}
         </div>
     )
 }
---
+++ components/TrainListItem.tsx
@@ -0,0 +1,127 @@
+import { TableRow, Table, TableBody, TableHeader, TableCell, TableColumn } from "@nextui-org/table"
+import { Card, CardBody, CardFooter } from "@nextui-org/card"
+import { Modal, ModalContent, ModalHeader, ModalBody, useDisclosure } from "@nextui-org/modal"
+import { Snippet } from "@nextui-org/snippet"
+import { bartStationIDs } from "@/config/lists"
+import moment from "moment"
+import CountdownArrival from "@/components/CountdownArrival"
+
+export default function TrainListItem({ train }: { train: any  }) {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    console.log(train)
+
+    let dep = moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000);
+    let arvl;
+    let diffInSeconds;
+
+    if (train.stopTimeUpdate[0]?.arrival?.time) {
+        arvl = moment(parseInt(train.stopTimeUpdate[0].arrival.time) * 1000);
+        diffInSeconds = dep.diff(arvl, 'minute'); // Difference in seconds
+    } else {
+        diffInSeconds = null;  // or 'undefined', or 'N/A', or any other value you'd see fit
+    }
+
+    console.log(`Difference in minutes: ${diffInSeconds}`);
+
+
+    // @ts-ignore
+    if (moment(parseInt(train.stopTimeUpdate[0].arrival.time) * 1000).isBefore(moment())) {
+        return
+    }
+
+
+
+
+    console.log(train)
+
+    // @ts-ignore
+    let arrivalTime = moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000)
+
+    // @ts-ignore
+    if (train?.delayed && train.stopTimeUpdate[0].departure.delay < 1) {
+        // @ts-ignore
+        train.delayed = false
+    }
+
+    // @ts-ignore
+    let lastStop = bartStationIDs[String(train.stopTimeUpdate[train.stopTimeUpdate.length - 1].stopId).toLowerCase()]
+
+    return (
+        <>
+            {/*@ts-ignore*/}
+            <Card className="w-[300px] m-3 mr-3 rounded-xl " isPressable isHoverable key={train.trip.tripId} onPress={onOpen}>
+                <CardBody className="flex gap-3">
+                    <div className="flex flex-col">
+                        {/*@ts-ignore*/}
+                        <p className="text-md">{bartStationIDs[String(train.stopTimeUpdate[0].stopId).toLowerCase()]}</p>
+
+                        <p className="text-small text-default-500">
+                            <CountdownArrival arrivalTime={arrivalTime} />
+                            {/*@ts-ignore*/}
+                            {train?.delayed && <span className="text-red-500"> +{parseInt(train.stopTimeUpdate[0].departure.delay / 6).toFixed(0)} minutes late</span>}
+
+                        </p>
+                        {/*@ts-ignore*/}
+                        <p className="text-small text-default-500">{moment(parseInt(train.stopTimeUpdate[0].departure.time) * 1000).fromNow()}</p>
+
+                    </div>
+                </CardBody>
+                <CardFooter className="text-small text-default-500 text-center w-max">
+
+                    {!lastStop && (
+                        <>
+                            Departing from this station
+                        </>
+                    )}
+                    {lastStop && (
+                        <>
+                            {/*@ts-ignore*/}
+                            Going to {bartStationIDs[String(train.stopTimeUpdate[train.stopTimeUpdate.length - 1].stopId).toLowerCase()]}
+                        </>
+                    )}
+
+                </CardFooter>
+
+            </Card>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} key={`${train.trip.tripId}-m`} scrollBehavior={'inside'} className={""}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Trip #{train.trip.tripId}</ModalHeader>
+                            <ModalBody>
+                                <Table hideHeader isCompact removeWrapper fullWidth>
+                                    <TableHeader>
+                                        <TableColumn width={0.5}>Stop Sequence</TableColumn>
+                                        <TableColumn>T</TableColumn>
+                                        <TableColumn>Station</TableColumn>
+                                    </TableHeader>
+                                    <TableBody>
+                                        {train.stopTimeUpdate.map((stopTime: any) => {
+                                            let delayed = false
+                                            if (stopTime.departure.delay > 0) {
+                                                // @ts-ignore
+                                                delayed = true
+                                            }
+
+                                            return (
+                                                <TableRow key={stopTime.stopId} className={'text-xs m-0'}>
+                                                    <TableCell className={'text-tiny text-foreground-400 '}>{stopTime.stopSequence} </TableCell>
+                                                    <TableCell>
+                                                        <Snippet symbol="" variant="flat" color={delayed ? "danger" : "default"} hideCopyButton>{moment(parseInt(stopTime.departure.time) * 1000).toDate().toLocaleTimeString()}</Snippet>
+
+                                                    </TableCell>
+                                                    {/* @ts-ignore */}
+                                                    <TableCell>{bartStationIDs[String(stopTime.stopId).toLowerCase()]}</TableCell>
+                                                </TableRow>
+                                            )
+                                        })}
+                                    </TableBody>
+                                </Table>
+                            </ModalBody>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
