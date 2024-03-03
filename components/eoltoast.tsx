37fdd167ec4e609b2228dc6c7939c026ce49f059
quacksire <sam@quacksire.dev>
2024-03-03 02:26:24 -0800

    bottom right works better
    Signed-off-by: quacksire <sam@quacksire.dev>

--- components/eoltoast.tsx
+++ components/eoltoast.tsx
@@ -10,7 +10,7 @@ export default function EolToast() {
         setTimeout(() => {
             toast.error(`Effective ${new Date(Date.parse("2024-03-04T08:00:00.000Z")).toLocaleDateString()}, BART will remove all legacy trains from their fleet, ending the cars' 51 years of service. `, {
                 important: true,
-                position: "top-center",
+                position: "bottom-right",
                 // basically forever in seconds, 60 * 60 * 24 * 365 * 3 is too large for a number
                 duration: 60 * 60 * 24 * 365 * 3,
                 cancel: {
